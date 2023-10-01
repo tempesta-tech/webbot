4c9c183196435ddb5ecdd83b48e15059e9c78886
Alexander Krizhanovsky <ak@tempesta-tech.com>
2023-10-01 18:33:27 +0400

    Initial commit to find broken links

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # webbot
-Website crawler for performance and security tasks
+
+Website crawler for performance and security tasks:
+* reveal dead links on a target website
+* warm web accelerator's cache
+* emulate bots behaviour (e.g. scrappers) to test a bots mitigation software
+
+
+## Installation
+
+Prerequisites for Ubuntu 22:
+```sh
+sudo apt update
+sudo apt install -y unzip xvfb libxi6 libgconf-2-4
+sudo apt install default-jdk
+sudo curl -sS -o - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add
+sudo bash -c "echo 'deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main' >> /etc/apt/sources.list.d/google-chrome.list"
+sudo apt -y update
+sudo apt -y install google-chrome-stable
+```
+
+[Download](https://chromedriver.storage.googleapis.com/index.html) the latest Chrome
+driver and install it:
+```sh
+unzip chromedriver_linux64.zip
+mv chromedriver /usr/bin/chromedriver 
+chown root:root /usr/bin/chromedriver
+chmod +x /usr/bin/chromedriver
+```
+
+Install the [Selenium Python driver](https://pypi.org/project/selenium/) with:
+```sh
+pip install selenium
+```
+
+You might experience exception
+```
+selenium.common.exceptions.SessionNotCreatedException: Message: session not created: This version of ChromeDriver only supports Chrome version 114
+Current browser version is 117.0.5938.132 with binary path /usr/bin/google-chrome
+```
+in this case you should install a Chrome or Chromium version of matching major
+version number. [Here](https://chromium.cypress.io/) the list of old binary versions
+of the browser.
+
+You can run `wbot` with the custom browser like
+```sh
+./wbot.py --crome_bin /opt/google/chromium-114/chrome
+```
+
+## References
+
+* [Selenium documentation](https://www.selenium.dev/documentation/), including Python API
---
+++ wbot.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+import argparse
+import logging
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+import time
+
+__author__ = "Tempesta Technologies, Inc."
+__copyright__ = "Copyright (C) 2023 Tempesta Technologies, Inc."
+__license__ = "GPL2"
+
+
+arg_parser = argparse.ArgumentParser(description="Website crawler for performance and security tasks")
+arg_parser.add_argument("--log", action="store", type=str, default='info', help="Logging verbosity (debug, info, warning, error, critical)")
+arg_parser.add_argument("--chrome_bin", action="store", type=str, default=None, help="Custom path to Chrome browser binary")
+arg_parser.add_argument("--domain", action="store", type=str, default="tempesta-tech.com", help="Domain to explore")
+arg_parser.add_argument("--broken", action="store", type=int, default=0, help="Check broken URLs")
+args = arg_parser.parse_args()
+
+retrieved = {}
+
+def to_retrieve(url):
+    """ Rules which URLs we should retrieve.
+    At the moment only not visited yet.
+    """
+    if url in retrieved:
+        return False
+    retrieved[url] = 1
+    return True
+
+def retrieve(drv, domain, url):
+    logging.info(f'retrieve {url}')
+
+    drv.get(url)
+
+    try:
+        # Get all links from the page.
+        links = drv.find_elements(By.CSS_SELECTOR, "a")
+    except selenium.common.exceptions.TimeoutException:
+        logging.warning(f'could not load links for {url}')
+        return
+
+    for l in links:
+        u = l.get_attribute('href')
+        if not u:
+            continue
+
+        # Get the status with requests library and then retrieve the URL again
+        # recursively with Selenium driver. We need the double requests for now
+        # becauseit's not easy to get response status from selenium.
+        # Trust self-signed certificates.
+        logging.info(f'get {u}')
+        try:
+            result = requests.get(u, verify=False)
+            if result.status_code != 200:
+                logging.warning(f'BROKEN LINK {u} on {url} (status {result.status_code})')
+        except:
+            logging.warning(f'BROKEN LINK {u} on {url} (can not connect)')
+
+        if '://{domain}/' in u and to_retrieve(u):
+            retrieve(drv, domain, u)
+
+def run_crawler(domain):
+    options = webdriver.ChromeOptions()
+    if args.chrome_bin:
+        options.binary_location = args.chrome_bin
+    # Don't care about certificates validity and accept self-signed certificates.
+    options.add_argument('ignore-certificate-errors')
+
+    drv = webdriver.Chrome(service=Service('/usr/bin/chromedriver'), options=options)
+
+    retrieve(drv, domain, f'https://{domain}/')
+
+if __name__ == '__main__':
+    log_lvl = getattr(logging, args.log.upper(), None)
+    if not isinstance(log_lvl, int):
+        raise ValueError('Invalid log level: %s' % args.log)
+    logging.basicConfig(level=log_lvl)
+
+    # Ignore warnings about self-signed certificates.
+    import urllib3
+    urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+    run_crawler(args.domain)
